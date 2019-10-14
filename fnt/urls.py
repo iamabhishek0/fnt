b48ce7d5fa15707714ee27402fa389da28ec22d6
Abhishek <uchiha@pop-os.localdomain>
2019-10-14 16:10:06 +0530

    added counter have bnavigation bug

--- fnt/urls.py
+++ fnt/urls.py
@@ -19,5 +19,5 @@ from petition import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.petition, name = 'petition_index'),
+    path('', views.petition, name ='petition_index'),
 ]
--- petition/views.py
+++ petition/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from .forms import PetitionForm
+from .models import petition_entry
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 # Create your views here.
 
 
 def petition(request):
+    count = petition_entry.objects.all().count()
+    count = int(count)+5321
+    remaining = int(10000-count)
+    percent = int((count*100)/10000)
+
+
     if request.method == 'POST':
         form = PetitionForm(request.POST)
         if form.is_valid():
             form.save()
 
-            messages.success(request, "Successfully Created")
-            return HttpResponseRedirect('/')
+            # messages.success(request, "Successfully Created")
+            return render(request, 'petition_html/petition_success.html', {'count':count,'percent':percent,'remaining':remaining})
 
 
 
-    return render(request,'petition_html/index.html')
+    return render(request,'petition_html/index.html', {'count':count,'percent':percent,'remaining':remaining})
