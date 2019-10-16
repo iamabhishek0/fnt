d301675576e4aa6e95b509ea4835b4e133cc896e
Abhishek <uchiha@pop-os.localdomain>
2019-10-16 18:19:48 +0530

    db changed and synced

--- petition/models.py
+++ petition/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 class petition_entry(models.Model):
     name = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
+    phone_number = models.BigIntegerField()
     reason = models.CharField(max_length=1000)
     have_pet = models.BooleanField(null=True)
 
 
 class rescue_entry(models.Model):
     name = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
-    email = models.CharField(max_length=100)
+    phone_number = models.BigIntegerField()
+    email = models.EmailField(max_length=100)
     address = models.CharField(max_length=200)
-    scenario = models.CharField(max_length=1000)
-    identification_mark = models.CharField(max_length=100)
-    dog_type = models.CharField(max_length=100)
+    scenario = models.CharField(max_length=1000,blank=True)
+    identification_mark = models.CharField(max_length=100,blank=True)
+    dog_type = models.CharField(max_length=100,blank=True)
     date_time = models.DateTimeField(auto_now_add=True)
--- petition/views.py
+++ petition/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import PetitionForm,RescueForm
-from .models import petition_entry
+from .models import petition_entry,rescue_entry
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 # Create your views here.
@@ -15,22 +15,43 @@ def petition(request):
 
 
     if request.method =='POST':
-        form = PetitionForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = PetitionForm(request.POST)
+        # if form.is_valid():
+        #     form.save()
+        name = request.POST["name"]
+        phone_number = request.POST["phone_number"]
+        reason = request.POST["reason"]
+        have_pet = request.POST["have_pet"]
+
+        petition = petition_entry(name = name, phone_number= phone_number, reason=reason, have_pet= have_pet)
+        petition.save()
 
             # messages.success(request, "Successfully Created")
-            return render(request,'petition_html/petition_success.html', {'count':count, 'percent':percent, 'remaining':remaining})
+        return render(request,'petition_html/petition_success.html', {'count':count, 'percent':percent, 'remaining':remaining})
 
             # return redirect('petition_index')
     return render(request, 'petition_html/index.html', {'count':count,'percent':percent,'remaining':remaining})
 
 def rescue(request):
     if request.method =='POST':
-        form = RescueForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # ['name', 'phone_number', 'email', 'address', 'scenario', 'identification_mark', 'dog_type', ]
 
-            return render(request, 'petition_html/rescue.html')
+        # if form.is_valid():
+        #     form.save()
+
+        name = request.POST["name"]
+        phone_number = request.POST["phone_number"]
+        email=request.POST["email"]
+        address = request.POST["address"]
+        scenario = request.POST["scenario"]
+        identification_mark = request.POST["identification_mark"]
+        dog_type = request.POST["dog_type"]
+
+        rescue = rescue_entry(name=name,phone_number=phone_number,email=email,address=address,scenario=scenario,identification_mark=identification_mark,dog_type=dog_type)
+        rescue.save()
+
+
+
+        return render(request, 'petition_html/rescue.html')
 
     return render(request, 'petition_html/rescue.html')
