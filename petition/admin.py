d9f5fe850d7850737476a4761499d72995ebbe6a
Abhishek <uchiha@pop-os.localdomain>
2019-10-18 00:27:45 +0530

    admin panel

--- petition/admin.py
+++ petition/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import petition_entry,rescue_entry
 
 
 # Register your models here.
-admin.site.register(petition_entry)
+# admin.site.register(petition_entry)
+#
+# class PetitionAdmin(admin.ModelAdmin):
+#
+#     list_display= ['name', 'phone_number', 'have_pet','reason', ]
+#     class Meta:
+#         model = petition_entry
+
+# admin.site.unregister(petition_entry)
+
+# @admin.register(petition_entry)
+class PetitionAdmin(ImportExportModelAdmin):
+    model = petition_entry
+    list_display = ['name', 'phone_number', 'have_pet','reason', ]
+    export_order = ['name', 'phone_number', 'have_pet','reason', ]
+admin.site.register(petition_entry,PetitionAdmin)
 
 admin.site.register(rescue_entry)
